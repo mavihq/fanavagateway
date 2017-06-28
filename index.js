c0ab885751a3911aa8df5eb5e919cbd0a4378177
shk <shaahin.karimpour902@gmail.com>
2017-06-28 13:31:25 +0430

    bug fix

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /**
  * Copyright 2017-present, Mavi, Inc.
  * All rights reserved.
@@ -9,191 +15,176 @@
  * @providesModule FanavaGateway
  */
 
-const soap = require('soap');
-const moment = require('moment');
-const config = require('./config.json');
+var soap = require('soap');
+var moment = require('moment');
+var config = require('./config.json');
 /**
  * Fanava gateway helper
  */
-class FanavaGateway {
 
-    constructor(username, password, mercahantID, redirectURL) {
-        this.username = username
-        this.password = password
-        this.mercahantID = mercahantID
-        this.redirectURL = redirectURL
-        this.SESSION_ID = null
+var FanavaGateway = function () {
+    function FanavaGateway(username, password, mercahantID, redirectURL) {
+        _classCallCheck(this, FanavaGateway);
+
+        this.username = username;
+        this.password = password;
+        this.mercahantID = mercahantID;
+        this.redirectURL = redirectURL;
+        this.SESSION_ID = null;
     }
 
     /**
      * Initialize the client
      */
-    init() {
-        const _self = this
-        return new Promise((resolve, reject) => {
-            soap
-                .createClient(config.webservice_url, function (err, client) {
-                    if (err) 
-                        return reject(_self._processError(err));
+
+
+    _createClass(FanavaGateway, [{
+        key: 'init',
+        value: function init() {
+            var _self = this;
+            return new Promise(function (resolve, reject) {
+                soap.createClient(config.webservice_url, function (err, client) {
+                    if (err) return reject(_self._processError(err));
                     resolve(client);
-                })
-        });
-    }
+                });
+            });
+        }
+    }, {
+        key: 'prepareFormHiddenInputs',
+        value: function prepareFormHiddenInputs(amount, resNum) {
+            var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
 
-    prepareFormHiddenInputs(amount, resNum, options = {}) {
-        return `
-            <input type='hidden' name="Amount" value="${amount}"/>
-            <input type='hidden' name="resNum" value="${resNum}"/>
-            <input type='hidden' name="MID" value="${this.mercahantID}"/>
-            <input type='hidden' name="‫‪redirectURL‬‬" value="${this.redirectURL}"/>
-            ${Object.keys(options).map(key => `<input type="hidden" name="${key}" value="${options[key]}" />`).join('\n')}
-        `
-    }
+            return '\n            <input type=\'hidden\' name="Amount" value="' + amount + '"/>\n            <input type=\'hidden\' name="resNum" value="' + resNum + '"/>\n            <input type=\'hidden\' name="MID" value="' + this.mercahantID + '"/>\n            <input type=\'hidden\' name="‫‪redirectURL‬‬" value="' + this.redirectURL + '"/>\n            ' + Object.keys(options).map(function (key) {
+                return '<input type="hidden" name="' + key + '" value="' + options[key] + '" />';
+            }).join('\n') + '\n        ';
+        }
+    }, {
+        key: 'getFormUrl',
+        value: function getFormUrl() {
+            return config.form_url;
+        }
+    }, {
+        key: 'getSimplePaymentForm',
+        value: function getSimplePaymentForm(amount, resNum) {
+            var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+            var message = arguments.length <= 3 || arguments[3] === undefined ? "در حال انتقال به درگاه..." : arguments[3];
 
-    getFormUrl() {
-        return config.form_url
-    }
-
-    getSimplePaymentForm(amount, resNum, options = {}, message = "در حال انتقال به درگاه...") {
-        const hiddenInps = this.prepareFormHiddenInputs(amount, resNum, options)
-        const url = this.getFormUrl()
-        return `
-            <!DOCTYPE html>
-            <html>
-
-            <head>
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <title>${message}</title>
-            </head>
-
-            <body>
-                <div style="text-align: center;width: 100%;height: 100%;">
-                    <h2>${message}</h2>
-                    <form id="form" method="post" action="${url}">
-                        ${hiddenInps}
-                    </form>
-                    <script type="text/javascript">
-                        document.getElementById("form").submit()
-                    </script>
-                </div>
-            </body>
-
-            </html>
-        `
-    }
-
-    login() {
-        const _self = this
-        return new Promise((resolve, reject) => {
-            _self
-                .init()
-                .then(function (client) {
+            var hiddenInps = this.prepareFormHiddenInputs(amount, resNum, options);
+            var url = this.getFormUrl();
+            return '\n            <!DOCTYPE html>\n            <html>\n\n            <head>\n                <meta charset="utf-8">\n                <meta http-equiv="X-UA-Compatible" content="IE=edge">\n                <meta name="viewport" content="width=device-width, initial-scale=1">\n                <title>' + message + '</title>\n            </head>\n\n            <body>\n                <div style="text-align: center;width: 100%;height: 100%;">\n                    <h2>' + message + '</h2>\n                    <form id="form" method="post" action="' + url + '">\n                        ' + hiddenInps + '\n                    </form>\n                    <script type="text/javascript">\n                        document.getElementById("form").submit()\n                    </script>\n                </div>\n            </body>\n\n            </html>\n        ';
+        }
+    }, {
+        key: 'login',
+        value: function login() {
+            var _self = this;
+            return new Promise(function (resolve, reject) {
+                _self.init().then(function (client) {
                     client.login({
                         username: _self.username,
                         password: _self.password
-                    }, (err, result) => {
-                        if (err) 
-                            return reject(_self._processError(err));
-                        _self.SESSION_ID = result.return
-                        return resolve(client)
-                    })
-                })
-                .catch(function (err) {
+                    }, function (err, result) {
+                        if (err) return reject(_self._processError(err));
+                        _self.SESSION_ID = result.return;
+                        return resolve(client);
+                    });
+                }).catch(function (err) {
                     return reject(_self._processError(err));
                 });
-        })
-    }
+            });
+        }
+    }, {
+        key: '_processError',
+        value: function _processError(error) {
+            error.message = error.message.replace('S:Server: ', '');
+            return error;
+        }
 
-    _processError(error) {
-        error.message = error
-            .message
-            .replace('S:Server: ', '')
-        return error
-    }
+        /**
+         * Verifies the payment
+         */
 
-    /**
-     * Verifies the payment
-     */
-    verify(refNum, amount) {
-        var _self = this;
-        return new Promise((resolve, reject) => {
-            _self
-                .login()
-                .then(function (client) {
-                    const args = {
+    }, {
+        key: 'verify',
+        value: function verify(refNum, amount) {
+            var _self = this;
+            return new Promise(function (resolve, reject) {
+                _self.login().then(function (client) {
+                    var _this = this;
+
+                    var args = {
                         context: _self.makeContext(),
                         verifyRequest: {
                             refNumList: $refNum
                         }
-                    }
-                    client.verifyTransaction(args, (err, result) => {
-                        if (err) 
-                            return reject(this._processError(err))
-                        const response = result.return.verifyResponseResults
-                        const error = response.verificationError
-                        const _amount = response.verificationError.amount
-                        const _refNum = response.verificationError.refNum
-                        if(refNum == _refNum && amount == amount){
-                            resolve({ refNum, amount })
+                    };
+                    client.verifyTransaction(args, function (err, result) {
+                        if (err) return reject(_this._processError(err));
+                        var response = result.return.verifyResponseResults;
+                        var error = response.verificationError;
+                        var _amount = response.verificationError.amount;
+                        var _refNum = response.verificationError.refNum;
+                        if (refNum == _refNum && amount == amount) {
+                            resolve({ refNum: refNum, amount: amount });
                         }
                         reject({
                             message: error,
-                            code: error.message                           
-                        })
-                    })
-                })
-                .catch(function (err) {
-                    return reject({err: true, reason: err});
+                            code: error.message
+                        });
+                    });
+                }).catch(function (err) {
+                    return reject({ err: true, reason: err });
                 });
-        });
-    }
+            });
+        }
 
-    /**
-     * Reverse the payment
-     */
-    reverse(refNum, amount, resNum) {
-        var _self = this;
-        return new Promise((resolve, reject) => {
-            _self
-                .login()
-                .then(function (client) {})
-                .catch(function (err) {
+        /**
+         * Reverse the payment
+         */
+
+    }, {
+        key: 'reverse',
+        value: function reverse(refNum, amount, resNum) {
+            var _self = this;
+            return new Promise(function (resolve, reject) {
+                _self.login().then(function (client) {}).catch(function (err) {
                     return reject(_self._processError(err));
                 });
-        });
-    }
-
-    makeContext() {
-        return {
-            data: {
-                entry: {
-                    key: 'SESSION_ID',
-                    value: this.SESSION_ID
-                }
-            }
+            });
         }
-    }
-
-    logout() {
-        var _self = this;
-        return new Promise((resolve, reject) => {
-            _self
-                .login()
-                .then(function (client) {
-                    const args = {
-                        context: _self.makeContext()
+    }, {
+        key: 'makeContext',
+        value: function makeContext() {
+            return {
+                data: {
+                    entry: {
+                        key: 'SESSION_ID',
+                        value: this.SESSION_ID
                     }
-                    client.logout(args, (err, result) => {
-                        if (err) 
-                            return reject(this._processError(err))
-                    })
-                })
-                .catch(function (err) {
-                    return reject({err: true, reason: err});
+                }
+            };
+        }
+    }, {
+        key: 'logout',
+        value: function logout() {
+            var _self = this;
+            return new Promise(function (resolve, reject) {
+                _self.login().then(function (client) {
+                    var _this2 = this;
+
+                    var args = {
+                        context: _self.makeContext()
+                    };
+                    client.logout(args, function (err, result) {
+                        if (err) return reject(_this2._processError(err));
+                    });
+                }).catch(function (err) {
+                    return reject({ err: true, reason: err });
                 });
-        });
-    }
-}
+            });
+        }
+    }]);
+
+    return FanavaGateway;
+}();
+
 module.exports = FanavaGateway;
